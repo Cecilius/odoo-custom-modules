6e6d40c742ae75a7ea9a64e3ca2071759a69dd5d
Cecilius <legendarycecil@gmail.com>
2026-05-16 13:08:55 +0200

    added description to new field marking the repair team

--- repair_helpdesk/models/helpdesk_team.py
+++ repair_helpdesk/models/helpdesk_team.py
@@ -4,4 +4,8 @@ from odoo import fields, models
 class HelpdeskTeam(models.Model):
     _inherit = 'helpdesk.team'
 
-    x_repair_workflow_team = fields.Boolean(string='Repair Workflow Team', default=False)
+    x_repair_workflow_team = fields.Boolean(
+        string='Repair Workflow Team',
+        default=False,
+        help='Marks this Helpdesk team as the dedicated team for the repair workflow and related automations.'
+    )
