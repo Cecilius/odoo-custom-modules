2d38f7e355eccbfd13ed20299af60fd9691f60b8
Cecilius <legendarycecil@gmail.com>
2026-05-18 17:09:35 +0200

    adapted again

--- simplified_invoice/controllers/main.py
+++ simplified_invoice/controllers/main.py
@@ -1,18 +1,17 @@
 from odoo.http import request
 from odoo.addons.account.controllers.portal import PortalAccount
 
+
 class SimplifiedInvoicePortalAccount(PortalAccount):
     def _get_mandatory_billing_address_fields(self, country_sudo):
         field_names = super()._get_mandatory_billing_address_fields(country_sudo)
 
         if request.env.company.country_code == country_sudo.code == 'ES':
-            order = request.website.sale_get_order()
-            amount_total = order.amount_total if order else 0.0
-            partner = order.partner_id.commercial_partner_id if order and order.partner_id else request.env['res.partner']
+            order_id = request.session.get('sale_order_id')
+            order = request.env['sale.order'].sudo().browse(order_id) if order_id else request.env['sale.order']
+            amount_total = order.amount_total if order and order.exists() else 0.0
 
-            is_b2b = bool(partner.vat) or partner.company_type == 'company'
-
-            if not is_b2b and amount_total <= 400.0:
+            if amount_total <= 400.0:
                 field_names.discard('vat')
 
         return field_names
