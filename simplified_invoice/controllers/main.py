78d521549d505d9cdac22c1a60dd7053c4249ad4
Cecilius <legendarycecil@gmail.com>
2026-05-20 11:43:57 +0200

    trying total opposite of spanish localization

--- simplified_invoice/controllers/main.py
+++ simplified_invoice/controllers/main.py
@@ -4,21 +4,10 @@ from odoo.http import request
 
 class SimplifiedInvoicePortalAccount(PortalAccount):
     def _get_mandatory_billing_address_fields(self, country_sudo):
+        """Require VAT/NIF for Spanish customers in billing addresses on Spanish e-commerce."""
         field_names = super()._get_mandatory_billing_address_fields(country_sudo)
 
-        # Reuse the Spanish localization limit from the current company to keep checkout logic aligned with invoices.
-        if request.env.company.country_code == country_sudo.code == "ES":
-
-            field_names.discard("vat")
-
-        return field_names
-
-    def _get_mandatory_address_fields(self, country_sudo):
-        field_names = super()._get_mandatory_address_fields(country_sudo)
-
-        # Reuse the Spanish localization limit from the current company to keep checkout logic aligned with invoices.
-        if request.env.company.country_code == country_sudo.code == "ES":
-
-            field_names.discard("vat")
+        if request.env.company.country_code == country_sudo.code == 'ES':
+            field_names.discard('vat')
 
         return field_names
