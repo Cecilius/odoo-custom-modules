991cc8dbf3b41045f8c7182f529629f592bbf112
Cecilius <legendarycecil@gmail.com>
2026-08-22 15:19:15 +0200

    Delete resale_ai_intake directory

--- resale_ai_intake/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import ai_configuration
-from . import product_product
-from . import acquisition_batch
-from . import intake_wizard
-from . import input_wizard
-from . import research_wizard
-from . import name_line
